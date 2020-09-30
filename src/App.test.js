fd50bd697a77eef8245e9c338e4b874d75976150
Kevin St.Onge <kevinstonge@gmail.com>
2020-09-30 09:37:19 -0400

    ...

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { render } from "@testing-library/react";
+import { fetchShow as mockFetchShow } from "./api/fetchShow";
 import App from "./App";
+mockFetchShow();
+jest.mock("fetchShow", () => () => "abcde");
+
 test("renders", () => {
   const { getByTestId } = render(<App />);
   const loadingMessage = getByTestId("loadingMessage");
--- src/api/fetchShow.js
+++ src/api/fetchShow.js
@@ -1,9 +1,8 @@
 import axios from "axios";
-const fetchShow = () => {
+export const fetchShow = () => {
   return axios
     .get(
       "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
     )
     .then((r) => r.data);
 };
-export default fetchShow;
