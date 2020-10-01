0157910e46d6e443ec7cebbeb628c58ee88e7fd8
Kevin St.Onge <kevinstonge@gmail.com>
2020-10-01 00:22:42 -0400

    async tests for App

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,28 @@
 import React from "react";
-import { render } from "@testing-library/react";
-import { fetchShow as mockFetchShow } from "./api/fetchShow";
-import App from "./App";
-mockFetchShow();
-jest.mock("fetchShow", () => () => "abcde");
+import {
+  render,
+  act,
+  fireEvent,
+  getByTestId,
+  wait,
+} from "@testing-library/react";
 
-test("renders", () => {
+import { fetchShow as mockFetchShow } from "./api/fetchShow";
+import mockFetchShowData from "./testing/mockFetchShowData";
+import App from "./App";
+
+jest.mock("./api/fetchShow");
+mockFetchShow.mockResolvedValue(mockFetchShowData);
+
+it("displays loading message on initial load", async () => {
   const { getByTestId } = render(<App />);
   const loadingMessage = getByTestId("loadingMessage");
   expect(loadingMessage).toHaveTextContent("Fetching data...");
+});
+
+it("displays show name after fetching show", async () => {
+  const { getByTestId } = render(<App />);
+  await wait();
+  const showName = getByTestId("showName");
+  expect(showName).toHaveTextContent("Stranger Things");
 });
